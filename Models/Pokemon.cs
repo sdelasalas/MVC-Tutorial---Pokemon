c1f78f8453a2d903c5668f40b2d8f4470c672bab
sdelasalas <stacy@Alderaan.fios-router.home>
2020-04-26 16:57:05 -0400

    add data model class

---
+++ Models/Pokemon.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcPokemon.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int HP { get; set; }
+        public string EvolvesFrom { get; set; }
+    }
+}
