8227da75f0a37648692d76f3ce556fd277f8bc59
sdelasalas <stacy@Alderaan.fios-router.home>
2020-04-26 22:43:15 -0400

    split SeedData and Pokemon class

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -1,46 +1,10 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using MvcPokemon.Data;
+
 using System;
-using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcPokemon.Models
 {
-    public static class SeedData
-    {
-        public static void Initialize(IServiceProvider service Provider)
-        {
-            using (var context = new MvcPokemonContext(
-                ServiceProvider.GetRequiredService<DbContextOptions<MvcPokemonContext>>()))
-                {
-                    //Look for Pokemon
-                    if (context.Pokemon.Any())
-                    {
-                        return; //DB has been seeded
-                    }
-
-                    context.Pokemon.AddRange(
-                        new Pokemon
-                        {
-                            Name = "Bulbasaur",
-                            Type = "Grass",
-                            HP = Convert.ToInt32("200"),
-                            EvolvesFrom = "A rock"
-                        },
-
-                        new Pokemon
-                        {
-                            Name = "Machomp",
-                            Type = "Fighting",
-                            HP = Convert.ToInt32("100"),
-                            EvolvesFrom = "Machoke"  
-                        }
-                    );
-                    context.SaveChanges();
-                }
-            }
-        }
-    }
+    
     public class Pokemon
     {
         public int Id { get; set; }
@@ -49,3 +13,4 @@ namespace MvcPokemon.Models
         public int HP { get; set; }
         public string EvolvesFrom { get; set; }
     }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcPokemon.Data;
+using System;
+using System.Linq;
+
+namespace MvcPokemon.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcPokemonContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MvcPokemonContext>>()))
+                {
+                    //Look for Pokemon
+                    if (context.Pokemon.Any())
+                    {
+                        return; //DB has been seeded
+                    }
+
+                    context.Pokemon.AddRange(
+                        new Pokemon
+                        {
+                            Name = "Bulbasaur",
+                            Type = "Grass",
+                            HP = Convert.ToInt32("200"),
+                            EvolvesFrom = "A rock"
+                        },
+
+                        new Pokemon
+                        {
+                            Name = "Machomp",
+                            Type = "Fighting",
+                            HP = Convert.ToInt32("100"),
+                            EvolvesFrom = "Machoke"  
+                        }
+                    );
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
