56966e781dc48494abb3a0222f35a241d5ad8bfe
sdelasalas <stacy@Alderaan.fios-router.home>
2020-04-27 10:41:07 -0400

    adding validation - not complete

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -9,12 +9,23 @@ namespace MvcPokemon.Models
     public class Pokemon
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength =3)]
+        [Required]
         public string Name { get; set; }
+        
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
         public string Type { get; set; }
         public int HP { get; set; }
 
         [Display(Name = "Evolves From")]
         [DataType(DataType.Text)]
+        
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
         public string EvolvesFrom { get; set; }
 
         public string Rating {get; set;}
