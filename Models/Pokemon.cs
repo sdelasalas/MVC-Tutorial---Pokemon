e3e2a94279875873a24e7094f3b08f8c1aff3a96
sdelasalas <stacy@Alderaan.fios-router.home>
2020-04-26 21:34:34 -0400

    seeded the database

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -1,8 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcPokemon.Data;
 using System;
-using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace MvcPokemon.Models
 {
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider service Provider)
+        {
+            using (var context = new MvcPokemonContext(
+                ServiceProvider.GetRequiredService<DbContextOptions<MvcPokemonContext>>()))
+                {
+                    //Look for Pokemon
+                    if (context.Pokemon.Any())
+                    {
+                        return; //DB has been seeded
+                    }
+
+                    context.Pokemon.AddRange(
+                        new Pokemon
+                        {
+                            Name = "Bulbasaur",
+                            Type = "Grass",
+                            HP = Convert.ToInt32("200"),
+                            EvolvesFrom = "A rock"
+                        },
+
+                        new Pokemon
+                        {
+                            Name = "Machomp",
+                            Type = "Fighting",
+                            HP = Convert.ToInt32("100"),
+                            EvolvesFrom = "Machoke"  
+                        }
+                    );
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
     public class Pokemon
     {
         public int Id { get; set; }
@@ -11,4 +49,3 @@ namespace MvcPokemon.Models
         public int HP { get; set; }
         public string EvolvesFrom { get; set; }
     }
-}
